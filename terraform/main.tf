99e5fd2d9a2d6375a6a0adbed9f176adb96fdd76
Craig Dent <cgdent@yahoo.com>
2026-05-12 18:14:45 +1000

    Add server-side encryption to demo S3 bucket
    Added server-side encryption configuration to the demo S3 bucket.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,24 @@
 resource "aws_s3_bucket" "demo_bucket" {
   bucket = var.bucket_name
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
-    Name        = "terraform-demo"
+    Name = "terraform-demo"
+  }
+}
+
+resource "aws_s3_bucket" "demo_bucket_secondary" {
+  bucket = "craigdent-demo-secondary-bucket-2026"
+  
+  tags = {
+    Name        = "terraform-demo-secondary"
     Environment = "demo"
-    Owner       = "cgdent"
   }
 }
